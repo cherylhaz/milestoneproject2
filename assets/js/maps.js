f2c412dcfced3f406054d434869d2f95ea1a85d5
cherylhaz <cheryl.hazenberg@gmail.com>
2020-04-28 08:34:02 +0000

    map working with sample google earthquate data

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,6 +1,5 @@
-  
+ /*
 var xhr = new XMLHttpRequest();
-
 function dataCall(){
 $.ajax({
     xhrFields: {
@@ -10,16 +9,42 @@ $.ajax({
         xhr.setRequestHeader('Authorization', '200735964-55871b76fd696d0af0539bd9bc3b2dd6');
     },
 
-    url: "https://www.hikingproject.com/data",
+    url: "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200735964-55871b76fd696d0af0539bd9bc3b2dd6",
     type: "GET",
     data: {
-        "url" : 'https://www.hikingproject.com/data'
+        "url" : 'https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200735964-55871b76fd696d0af0539bd9bc3b2dd6'
     }
 }).done(function(data){
     console.log(data);
 });
-};
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 53.594270, lng: -7.061600},
-  zoom: 8
-});
+}
+*/
+
+var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 2,
+          center: new google.maps.LatLng(2.8,-187.3),
+          mapTypeId: 'terrain'
+        });
+    };
+
+        // Create a <script> tag and set the USGS URL as the source.
+        var script = document.createElement('script');
+        // This example uses a local copy of the GeoJSON stored at
+        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+        script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
+    
+
+      // Loop through the results array and place a marker for each
+      // set of coordinates.
+      window.eqfeed_callback = function(results) {
+        for (var i = 0; i < results.features.length; i++) {
+          var coords = results.features[i].geometry.coordinates;
+          var latLng = new google.maps.LatLng(coords[1],coords[0]);
+          var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+          });
+        }}
