b19371b65001fbef482bc50f79964c3a222765be
cherylhaz <cheryl.hazenberg@gmail.com>
2020-04-29 14:07:25 +0000

    Example trail id's displaying on page with results button

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,15 +4,31 @@
 //var switzerland = (lat: 46.8182, lon: 8.2275)
 //var spain = (lat: 40.4637, lon: 3.7492)
 
-function sendSearch(){
+    function getData(id,cb){
+
         var xhr = new XMLHttpRequest();
+
         xhr.open("GET", "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=5&key=200735964-55871b76fd696d0af0539bd9bc3b2dd6");
         xhr.send();
+
         xhr.onreadystatechange = function(){
             if (this.readyState == 4 && this.status == 200){
-                document.getElementById("data").innerHTML = this.responseText;
-            }};
-            
+              cb(JSON.parse(this.responseText));
+        }};
+    }
+    function writeToDocument(id){
+        var el = document.getElementById("data");
+        el.innerHTML = "";
+            getData(id, function(data){
+                data = data.trails;
+
+                data.forEach(function(item){
+                    el.innerHTML += "<p>" + item.name +"</p>";
+                })
+                
+            })
+    
+         
 //Code used from Google Tutorial - https://developers.google.com/maps/documentation/javascript/importing_data
       // Loop through the results array and place a marker for each
       // set of coordinates.
